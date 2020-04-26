ded72565e420213c3d6ded6eafcdf4753c71a182
Yuanchun <immr.shen@gmail.com>
2020-04-26 23:29:57 +0800

    update region

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 DBENGINE = 'mysql'  # ENGINE OPTIONS: mysql, sqlite3, postgresql
 DBNAME = 'ershoufang'
 DBUSER = 'root'
-DBPASSWORD = ''
+DBPASSWORD = 'as970218'
 DBHOST = '127.0.0.1'
 DBPORT = 3306
 CITY = 'cd'  # only one, shanghai=sh shenzhen=sh......
-REGIONLIST = [u'wuhou', u'xindu', u'tianfuxin']  # only pinyin support
+REGIONLIST = [u'xindou', u'tianfuxinqu', u'wuhou']  # only pinyin support
